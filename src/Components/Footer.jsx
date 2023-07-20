3361e72012cb7c83d7851488478eb967de32c252
Anirudh-Ghildiyal <b200094@nitsikkim.ac.in>
2023-07-20 21:32:41 +0530

    Updated Footer Links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,22 +1,42 @@
 import React from "react";
 import "../CSS/Footer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faYoutube, faTwitter } from "@fortawesome/free-brands-svg-icons";
-    
+import {
+  faFacebook,
+  faYoutube,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
 export default function Footer() {
   return (
-    <footer className="footer">
-        <div className="companySection ms-5">
-            <div>About Us</div>
-            <div className="ms-4">Contact Us</div>
-        </div>
+    <footer>
+      <div className="companySection ms-5">
+        <a
+          href="https://www.poyntsports.com/"
+          className="text-white text-decoration-none"
+        >
+          About Us
+        </a>
+        <a
+          href="https://www.poyntsports.com/"
+          className="text-white text-decoration-none ms-4"
+        >
+          Contact Us
+        </a>
+      </div>
       <div className="companyText">
         © 2023 Poyntsports.com, All Rights Reserved
       </div>
-      <div className="icons me-5">
-      <FontAwesomeIcon icon={faFacebook} className="facebookIcon " />
-      <FontAwesomeIcon icon={faYoutube} className="youtubeIcon ms-4" />
-      <FontAwesomeIcon icon={faTwitter} className="twitterIcon ms-4" />
+      <div className="me-5">
+        <a href="https://www.poyntsports.com/">
+          <FontAwesomeIcon icon={faFacebook} className="facebookIcon" />
+        </a>
+        <a href="https://www.poyntsports.com/">
+          <FontAwesomeIcon icon={faYoutube} className="youtubeIcon ms-4" />
+        </a>
+        <a href="https://www.poyntsports.com/">
+          <FontAwesomeIcon icon={faTwitter} className="twitterIcon ms-4" />
+        </a>
       </div>
     </footer>
   );
